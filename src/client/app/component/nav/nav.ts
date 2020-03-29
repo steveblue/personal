9ce57d8cc91831bcc969020ca3bfd48dbe505443
Steve Belovarich <steveblue@gmail.com>
2020-03-28 18:56:38 -0700

    prototype nav

--- src/client/app/component/nav/nav.ts
+++ src/client/app/component/nav/nav.ts
@@ -10,8 +10,20 @@ import template from './nav.html';
   template: template
 })
 class NavComponent extends CustomElement {
+  private isActive: boolean = false;
   constructor() {
     super();
+  }
+  connectedCallback() {
+    this.shadowRoot?.querySelector('.nav__button')?.addEventListener('click', this.toggle.bind(this));
+  }
+  toggle() {
+    this.isActive = this.isActive ? false : true;
+    if (this.isActive) {
+      this.shadowRoot?.querySelector('.nav__container')?.classList.add('is--open');
+    } else {
+      this.shadowRoot?.querySelector('.nav__container')?.classList.remove('is--open');
+    }
   }
 }
 
