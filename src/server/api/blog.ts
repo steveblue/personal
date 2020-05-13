cc8a59c43693f0c98e552118df799376a4d78257
Steve Belovarich <steveblue@gmail.com>
2020-05-12 22:30:16 -0700

    blog bugfixes

--- src/server/api/blog.ts
+++ src/server/api/blog.ts
@@ -2,12 +2,17 @@
 import { IRoute } from 'express';
 import { db } from './../init';
 
+function sortByDate(a,b){
+    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime() ? 1 : -1;
+}
+
 class BlogController implements IRoute {
     constructor() {
 
     }
     getPosts(req, res) {
-        res.status(200).send(db.getCollection('posts').data)
+        const data = db.getCollection('posts').data.sort(sortByDate);
+        res.status(200).send(data);
     }
 }
 
